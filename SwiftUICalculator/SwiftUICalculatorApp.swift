f8540c5fe05538336a2dfd7cdbbf9649d316a5b6
keerimsk <sakizkerim@outlook.com>
2024-07-30 16:06:22 +0300

    Initial Commit

---
+++ SwiftUICalculator/SwiftUICalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICalculatorApp.swift
+//  SwiftUICalculator
+//
+//  Created by Kerim Sakız on 30.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
